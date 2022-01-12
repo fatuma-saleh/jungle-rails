fd76251974a926c7c09456564c584ca08dd92e14
Fatuma <sfatex@gmail.com>
2022-01-12 18:34:25 -0500

    Create users model,controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
 
   
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create' 
 
   
   # The priority is based upon order of creation: first created -> highest priority.
