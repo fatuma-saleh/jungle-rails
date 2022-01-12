07f2747e1c89a9bff3c117a6aee28b4099a434be
Fatuma <sfatex@gmail.com>
2022-01-11 22:20:25 -0500

    Add route and controller for admin/categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,14 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:show,:create,:index]
+
   end
   get "about", to: "about#index"
+
+  
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
