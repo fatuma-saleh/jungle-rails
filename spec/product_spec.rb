869fa1548deca0788cef04ad80cb4103b1a7fc1c
Fatuma <sfatex@gmail.com>
2022-01-21 13:14:16 -0500

    Test that the expect error is found within the .errors.full_messages array

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -67,5 +67,17 @@ it "is not a Valid Product without a category" do
 expect(shirt).to_not be_valid
  end
 
+ it "will generate errors on create of products" do
+  @category = Category.create(name: "category1")
+  shirt = Product.create(
+      category: nil,
+      name:  'Gym shirt',
+      description: 'Athletic Basketball',
+      quantity: 17,
+      price: 100
+    )
+    puts shirt.errors.full_messages
+ expect(shirt.errors.full_messages).to eq(["Category can't be blank"]) 
+ end
 end
 end
