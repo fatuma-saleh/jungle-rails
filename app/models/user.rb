c3215aba18b114bb7cd25e471533ac3c16d58278
Fatuma <sfatex@gmail.com>
2022-01-21 23:02:17 -0500

    Define user validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :first_name,:last_name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, confirmation: true, length: {minimum: 6}
+
+  
+
+
 end
